293bc8d2848a22dbe788844ff4ba2d0044254f26
Simone Borelli <simone.borelli@aboutyou.com>
2021-05-27 22:02:18 +0200

    initial commit: cypress-grep not working example

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-# cypress-test-tiny
+### MODULE API
 
-> Tiny Cypress E2E test case
+<br>
 
-Build status | Name | Description
-:--- | :--- | :---
-[![CircleCI](https://circleci.com/gh/cypress-io/cypress-test-tiny.svg?style=svg)](https://circleci.com/gh/cypress-io/cypress-test-tiny) | CircleCI | Linux & Mac & Win 64
-[![Build status](https://ci.appveyor.com/api/projects/status/er7wpte7j00fsm8d/branch/master?svg=true)](https://ci.appveyor.com/project/cypress-io/cypress-test-tiny-fitqm/branch/master) | AppVeyor | Windows 32-bit
-[![Build status](https://ci.appveyor.com/api/projects/status/bpwo4jpue61xsbi5/branch/master?svg=true)](https://ci.appveyor.com/project/cypress-io/cypress-test-tiny/branch/master) | AppVeyor | Windows 64-bit
-[ ![Codeship Status for cypress-io/cypress-test-tiny](https://app.codeship.com/projects/98843020-d6d6-0135-402d-5207bc7a4d86/status?branch=master)](https://app.codeship.com/projects/263289) | Codeship Basic | Linux Docker
+### plugin cypress-grep
 
-## Important
+1.  @flaky
 
-Note that this project **DOES NOT** include Cypress dependency in the [package.json](package.json). The reason for such omission is that we use this project to test every Cypress build and do not want to spend time installing `cypress@x.x.x` just to immediately install and test `cypress@y.y.y`. Which means when submitting pull requests with a bug report, please save the problematic version of Cypress in `package.json`. Simply run `npm install --save-dev cypress` or `npm i -D cypress@x.x.x` and commit the change before submitting a pull request.
+### how to run all tests but the ones with tag `@flaky`
+`yarn launch --grepTags=-@flaky`
+
+### how to run all tests but the ones with tag `@flaky`
+`yarn launch --grepTags=@flaky`
+
+### how to run test with tag `@flaky` or `@bug`
+`yarn launch --grepTags="@flaky @bug"`
+
+### how to run test with tag `@flaky` and `@bug`
+`yarn launch --grepTags=@flaky+@bug`
---
+++ launch.ts
@@ -0,0 +1,104 @@
+import cypress from 'cypress';
+
+const enum Parameters {
+  command = 'command',
+  headed = 'headed',
+  browser = 'browser',
+  grepTags = 'grepTags',
+  htmlReport = 'report',
+}
+
+enum Commands {
+  run = 'run',
+  open = 'open',
+}
+
+const argv = require('yargs/yargs')(process.argv.slice(2))
+  .option(Parameters.command, {
+    type: 'string',
+    choices: Object.values(Commands),
+    default: Commands.open,
+  })
+  .option(Parameters.grepTags, {
+    type: 'string',
+    default: false,
+  })
+  .option(Parameters.headed, {
+    alias: ['h'],
+    type: 'boolean',
+    default: false,
+  })
+  .option(Parameters.htmlReport, {
+    type: 'boolean',
+    default: false,
+  })
+  .option(Parameters.browser, {
+    alias: ['b'],
+    type: 'string',
+    default: 'chrome',
+  })
+  .help().argv;
+
+const HEADED = argv[Parameters.headed];
+const BROWSER = argv[Parameters.browser];
+const COMMAND = argv[Parameters.command] ?? Commands.open;
+const GREP = argv[Parameters.grepTags];
+const CONFIG_PATH = 'cypress/config/configFiles/mobile.json';
+
+export interface CypressConfiguration {
+  browser?: string;
+  headless?: boolean;
+  spec?: string;
+  config: {
+    baseUrl?: string;
+    video?: boolean;
+    userAgent?: string;
+    viewportHeight?: number;
+    viewportWidth?: number;
+    integrationFolder?: string;
+    projectId?: string;
+  };
+  env: {
+    grep: string | undefined;
+  };
+  record: boolean;
+  parallel: boolean;
+  tag?: string;
+  key?: string;
+  configFile: string | false;
+  group?: string;
+}
+
+
+export const cypressConfig: CypressConfiguration = {
+  browser: BROWSER,
+  headless: !HEADED,
+  config: {
+    video: false,
+  },
+  env: {
+    grep: GREP
+  },
+  record: false,
+  parallel: false,
+  configFile: CONFIG_PATH,
+}
+
+const runCypress = () => {
+  if (COMMAND === Commands.run) {
+    return cypress.run(cypressConfig);
+  }
+  return cypress.open(cypressConfig);
+};
+
+runCypress()
+  .then(results => {
+    if (results.totalFailed > 0 || results.failures > 0) {
+      // Make sure to exit with an error code if tests failed
+      process.exit(1);
+    }
+  })
+  .catch(err => {
+    console.error(err.stack || err);
+    process.exit(1);
+  });
